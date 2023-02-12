99bf40991278ac9b3ebfbb774e726dd7702fc24c
karan-ba <112770702+karan-ba@users.noreply.github.com>
2023-02-12 13:51:57 +0530

    added navrang website folder

---
+++ navrang/js/script.js
@@ -0,0 +1,86 @@
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+};
+
+let shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingCart.classList.toggle("active");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+let loginForm = document.querySelector(".login-form");
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  loginForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+window.onescroll = () => {
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+// slider js for products
+
+var swiper = new Swiper(".product-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// slider js for customer's review
+
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
